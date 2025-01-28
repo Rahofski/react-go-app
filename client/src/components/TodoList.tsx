edc4882594580e1bf0a800dda74cdf963318ff5f
Rahofski <roma.rakhov15@gmail.com>
2025-01-28 20:37:44 +0300

    no task bug fixed

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -20,27 +20,32 @@ const TodoList = () => {
     const { data: todos, isLoading, error } = useQuery<Todo[]>({
         queryKey: ["todos"],
         queryFn: async () => {
-            if (!token) {
-                throw new Error("No tok provided"); // Проверяем наличие токена
-            }
+            try {
+                if (!token) {
+                    throw new Error("No token provided"); // Проверяем наличие токена
+                }
 
-            const res = await fetch(BASE_URL + "/todos", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`, // Добавляем токен в заголовок
-                },
-            });
-            const data = await res.json();
-            if (!res.ok) {
-                throw new Error(data.message || "Something went wrong");
+                const res = await fetch(BASE_URL + "/todos", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`, // Добавляем токен в заголовок
+                    },
+                });
+
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error(data.message || "Something went wrong");
+                }
+                return data || [];
+            } catch (error: any) {
+                console.error("Error fetching todos:", error);
+                throw error; // Пробрасываем ошибку для обработки в компоненте
             }
-            return data || [];
         },
     });
 
     if (error) {
-        console.error(error);
         return (
             <Flex justifyContent="center" alignItems="center" height="100vh">
                 <Text fontSize="xl" color="red.500">
@@ -48,6 +53,14 @@ const TodoList = () => {
                         ? "Войдите, чтобы создавать todo"
                         : "Failed to load tasks. Please try again later."}
                 </Text>
+            </Flex>
+        );
+    }
+
+    if (isLoading) {
+        return (
+            <Flex justifyContent="center" alignItems="center" height="100vh">
+                <Text fontSize="xl">Loading...</Text>
             </Flex>
         );
     }
